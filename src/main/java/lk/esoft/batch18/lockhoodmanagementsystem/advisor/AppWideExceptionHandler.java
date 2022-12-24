911bb5736a054e221608f924406b7165061d36d9
Acer <malingaweerasinghe0772163479@gmail.com>
2022-12-24 10:24:16 +0530

    project setup environment

---
+++ src/main/java/lk/esoft/batch18/lockhoodmanagementsystem/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,15 @@
+package lk.esoft.batch18.lockhoodmanagementsystem.advisor;
+import lk.esoft.batch18.lockhoodmanagementsystem.exception.NotFoundException;
+import lk.esoft.batch18.lockhoodmanagementsystem.util.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AppWideExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException(NotFoundException e){
+        return new ResponseEntity(new StandardResponse(404,"Error",e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+}
