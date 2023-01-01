9c4dea77d5b27d3769a4029a57ba7c708d8b9d5c
Acer <malingaweerasinghe0772163479@gmail.com>
2023-01-01 17:09:13 +0530

    user role case SECURITY

--- src/main/java/lk/esoft/batch18/lockhoodmanagementsystem/security/WebSecurityConfig.java
+++ src/main/java/lk/esoft/batch18/lockhoodmanagementsystem/security/WebSecurityConfig.java
@@ -1,113 +1,113 @@
-//package lk.esoft.batch18.lockhoodmanagementsystem.security;
-//
-//import lk.esoft.batch18.lockhoodmanagementsystem.security.jwt.AuthEntryPointJwt;
-//import lk.esoft.batch18.lockhoodmanagementsystem.security.jwt.AuthTokenFilter;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-////import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-////import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-////import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//import lk.esoft.batch18.lockhoodmanagementsystem.security.services.UserDetailsServiceImpl;
-//
-//@Configuration
-//@EnableGlobalMethodSecurity(
-//    // securedEnabled = true,
-//    // jsr250Enabled = true,
-//    prePostEnabled = true)
-//public class WebSecurityConfig  { // extends WebSecurityConfigurerAdapter {
-//  @Autowired
-//  UserDetailsServiceImpl userDetailsService;
-//
-//  @Autowired
-//  private AuthEntryPointJwt unauthorizedHandler;
-//
-//  @Bean
-//  public AuthTokenFilter authenticationJwtTokenFilter() {
-//    return new AuthTokenFilter();
+package lk.esoft.batch18.lockhoodmanagementsystem.security;
+
+import lk.esoft.batch18.lockhoodmanagementsystem.security.jwt.AuthEntryPointJwt;
+import lk.esoft.batch18.lockhoodmanagementsystem.security.jwt.AuthTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lk.esoft.batch18.lockhoodmanagementsystem.security.services.UserDetailsServiceImpl;
+
+@Configuration
+@EnableGlobalMethodSecurity(
+    // securedEnabled = true,
+    // jsr250Enabled = true,
+    prePostEnabled = true)
+public class WebSecurityConfig  { // extends WebSecurityConfigurerAdapter {
+  @Autowired
+  UserDetailsServiceImpl userDetailsService;
+
+  @Autowired
+  private AuthEntryPointJwt unauthorizedHandler;
+
+  @Bean
+  public AuthTokenFilter authenticationJwtTokenFilter() {
+    return new AuthTokenFilter();
+  }
+
+//  @Override
+//  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 //  }
-//
-////  @Override
-////  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-////    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-////  }
-//
+
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider() {
+      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+      authProvider.setUserDetailsService(userDetailsService);
+      authProvider.setPasswordEncoder(passwordEncoder());
+
+      return authProvider;
+  }
+
 //  @Bean
-//  public DaoAuthenticationProvider authenticationProvider() {
-//      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-//
-//      authProvider.setUserDetailsService(userDetailsService);
-//      authProvider.setPasswordEncoder(passwordEncoder());
-//
-//      return authProvider;
+//  @Override
+//  public AuthenticationManager authenticationManagerBean() throws Exception {
+//    return super.authenticationManagerBean();
 //  }
-//
-////  @Bean
-////  @Override
-////  public AuthenticationManager authenticationManagerBean() throws Exception {
-////    return super.authenticationManagerBean();
-////  }
-//
-//  @Bean
-//  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-//    return authConfig.getAuthenticationManager();
-//  }
-//
-//  @Bean
-//  public PasswordEncoder passwordEncoder() {
-//    return new BCryptPasswordEncoder();
-//  }
-//
-////  @Override
-////  protected void configure(HttpSecurity http) throws Exception {
-////    http.cors().and().csrf().disable()
-////      .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-////      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-////      .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-////      .antMatchers("/api/test/**").permitAll()
-////      .anyRequest().authenticated();
-////
-////    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-////  }
-////@Override
-////public void configure(WebSecurity web) throws Exception {
-////  web
-////          .ignoring()
-////          .mvcMatchers("/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**", "/v2/api-docs", "/webjars/**");
-////}
-//
-//  @Bean
-//  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    return authConfig.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
 //    http.cors().and().csrf().disable()
-//        .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-//        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-//        .authorizeRequests().antMatchers("/swagger-ui/**","/javainuse-openapi/**","/api/auth/**","/api/v1/company/**","/api/v1/plant/**","/api/v1/customer/**").permitAll()
-//        .antMatchers("/api/test/**").permitAll()
-//            .anyRequest().authenticated();
-//
-//
-//    http.authenticationProvider(authenticationProvider());
+//      .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+//      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//      .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+//      .antMatchers("/api/test/**").permitAll()
+//      .anyRequest().authenticated();
 //
 //    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//
-//    return http.build();
 //  }
-//
-//
-//
+//@Override
+//public void configure(WebSecurity web) throws Exception {
+//  web
+//          .ignoring()
+//          .mvcMatchers("/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**", "/v2/api-docs", "/webjars/**");
 //}
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.cors().and().csrf().disable()
+        .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        .authorizeRequests().antMatchers("/swagger-ui/**","/javainuse-openapi/**","/api/auth/**","/api/v1/company/**","/api/v1/plant/**","/api/v1/customer/**").permitAll()
+        .antMatchers("/api/test/**").permitAll()
+            .anyRequest().authenticated();
+
+
+    http.authenticationProvider(authenticationProvider());
+
+    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+
+
+
+}
